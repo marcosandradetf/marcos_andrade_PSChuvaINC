ff7bf4bd3e9da15662cdad25b1d825f1e22ac08a
Marcos Túlio <dev@marcostulio.in>
2023-10-09 13:53:59 -0300

    feat: finalizado header da pagina web

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DevChuva';
+  linkText: string = "PT, BR";
+
+  changeText1() {
+    this.linkText = "PT, BR";
+  }
+
+  changeText2() {
+    this.linkText = "EN, US";
+  }
+
+  changeText3() {
+    this.linkText = "ES, ES";
+  }
+
 }
